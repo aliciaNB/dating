6ea8017f456c498620e0285f448ed24bc9ebeeb2
Alicia Buehner <alicia.buehner@gmail.com>
2019-04-20 23:19:39 -0400

    Add route to homepage and first form

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!--
-Name: Alicia Buehner
-Date: 04.14.19
-Description: This file contains the index page for Dating I, instantiates the Fat-Free Framework
-             and defines the project routing.
- -->
 <?php
+//
+//Name: Alicia Buehner
+//Date: 04.14.19
+//Description: This file contains the index page for Dating I, instantiates the Fat-Free Framework
+//and defines the project routing.
+//
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -20,6 +20,18 @@ $f3 = Base::instance();
 $f3->route('GET /', function() {
     $view = new Template();
     echo $view-> render('views/home.html');
+});
+
+//Define home route
+$f3->route('GET /homepage', function() {
+    $view = new Template();
+    echo $view-> render('views/home.html');
+});
+
+//Define route to first create profile form - personal information
+$f3->route("POST /personalinformation", function() {
+    $view = new Template();
+    echo $view->render('views/personal_form.html');
 });
 
 //Run Fat-free
