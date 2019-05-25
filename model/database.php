a4a75664989689ea7783b9c2b05b6af67ad98e37
Alicia Buehner <alicia.buehner@gmail.com>
2019-05-25 13:09:09 -0400

    Add database class file

---
+++ model/database.php
@@ -0,0 +1,39 @@
+<?php
+//Name: Alicia Buehner
+//Date: 05.27.19
+//Description: This file contains the Database object class for the Dating Assignment.
+
+//TODO need require statement here...
+
+/**
+ * Class Database represents a new Database object
+ * for dating site user profiles.
+ *
+ * @author Alicia Buehner
+ * @copyright 2019
+ */
+class Database
+{
+    //field
+    private $_dbh;
+
+    function connect()
+    {
+        return;
+    }
+
+    function insertMember()
+    {
+        return;
+    }
+
+    function getMember($member_id)
+    {
+        return;
+    }
+
+    function getInterests($member_id)
+    {
+        return;
+    }
+}
