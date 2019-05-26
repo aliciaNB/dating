a95b36821fca2bb6f67b2dbe10b7c62ac71c84eb
Alicia Buehner <alicia.buehner@gmail.com>
2019-05-25 23:56:11 -0400

    Moved file

--- model/config-member.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Name: Alicia Buehner
-//Date: 05.27.19
-//Description: This file contains the database user configuration file. This file lives above
-//             above the public_html file in home/abuehner/
-
-define("DB_DSN", "mysql:dbname=abuehner_it328");
-define("DB_USERNAME", "abuehner_member");
-define("DB_PASSWORD", "1t!5mBtkG9yQ");
